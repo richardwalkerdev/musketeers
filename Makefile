e6880c34067c8c61ee7d8ff7746cee37210fd792
Richard Walker <rwalker@redhat.com>
2020-03-24 09:51:34 +0000

    added script

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 echo:
-	docker-compose run --rm alpine echo 'Hello, World!'
+	docker-compose run --rm alpine echo 'OpenShift Pipeline'
+
+.PHONY: compare
+compare: 
+	echo "Comparing versions."
+	./scripts/compare-versions.sh
---
+++ scripts/get-latest.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+# pass in --version-only as first arg to not download
+# will come back to this and make it less lazy
+
+ACCOUNT_PATH='/home/ec2-user'
+
+
+LATEST="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.3/release.txt"
+
+LATEST_VERSION=`curl -s ${LATEST} | grep Version: | sed 's/ *Version: *//g'`
+
+if [[ $1 == "--version-only" ]]; then
+    echo "${LATEST_VERSION}"
+fi
+
+echo $LATEST_VERSION > latest-version.txt
+
+# Get current version
+CURRENT_VERSION=`~/bin/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
+
+if [[ $1 == "--version-only" ]]; then
+    echo "${CURRENT_VERSION}"
+fi
+
+echo $CURRENT_VERSION > current-version.txt
+
+if [[ ${CURRENT_VERSION} != ${LATEST_VERSION} ]]; then
+    echo "Newer version found, deploy required" && touch ${ACCOUNT_PATH}/continue;
+else
+    echo "Matching versions, no changes required" && touch ${ACCOUNT_PATH}/stop;
+fi
