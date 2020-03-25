7d12ddc8acc730f1d5076c8156d01791e1616a77
Richard Walker <rwalker@redhat.com>
2020-03-25 11:45:41 +0000

    testing custom image

--- scripts/compare-versions.sh
+++ scripts/compare-versions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # /opt/temp/
-echo "SOME TEXT opt temp" > /opt/temp/testing-temp.txt
+#echo "SOME TEXT opt temp" > /opt/temp/testing-temp.txt
 
 # /var/lib/buildkite-agent/builds/ip-10-10-0-9-eu-west-2-compute-internal-1/musketeers/testing-musketeers/
-echo "some text" > testing-working-dir.txt
+#echo "some text" > testing-working-dir.txt
 
 echo "ACCOUNT_PATH = ${ACCOUNT_PATH}"
 
@@ -14,14 +14,12 @@ LATEST_VERSION=`curl -s ${LATEST} | grep Version: | sed 's/ *Version: *//g'`
 
 echo $LATEST_VERSION > /opt/temp/latest-version.txt
 
-# # Get current version
-# #CURRENT_VERSION=`/opt/app/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
+# Get current version
+CURRENT_VERSION=`/opt/temp/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
+echo $CURRENT_VERSION > /opt/temp/current-version.txt
 
-
-# echo $CURRENT_VERSION > current-version.txt
-
-# if [[ ${CURRENT_VERSION} != ${LATEST_VERSION} ]]; then
-#     echo "Newer version found, deploy required" && touch ${ACCOUNT_PATH}/continue;
-# else
-#     echo "Matching versions, no changes required" && touch ${ACCOUNT_PATH}/stop;
-# fi
+if [[ ${CURRENT_VERSION} != ${LATEST_VERSION} ]]; then
+    echo "Newer version found, deploy required" && touch ${ACCOUNT_PATH}/continue;
+else
+    echo "Matching versions, no changes required" && touch ${ACCOUNT_PATH}/stop;
+fi
