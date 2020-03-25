488302460ccaa31825531adc7b6001bcd2a788e5
Richard Walker <rwalker@redhat.com>
2020-03-25 11:40:06 +0000

    testing custom image

--- scripts/compare-versions.sh
+++ scripts/compare-versions.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# /opt/temp/
 echo "SOME TEXT opt temp" > /opt/temp/testing-temp.txt
+
+# /var/lib/buildkite-agent/builds/ip-10-10-0-9-eu-west-2-compute-internal-1/musketeers/testing-musketeers/
 echo "some text" > testing-working-dir.txt
 
-# echo "ACCOUNT_PATH = ${ACCOUNT_PATH}"
+echo "ACCOUNT_PATH = ${ACCOUNT_PATH}"
 
-# LATEST="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.3/release.txt"
+LATEST="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.3/release.txt"
 
-# LATEST_VERSION=`curl -s ${LATEST} | grep Version: | sed 's/ *Version: *//g'`
+LATEST_VERSION=`curl -o /opt/temp/openshift-install -s ${LATEST} | grep Version: | sed 's/ *Version: *//g'`
 
-# echo $LATEST_VERSION > latest-version.txt
-# echo $LATEST_VERSION > ${ACCOUNT_PATH}/test.txt
+echo $LATEST_VERSION > /opt/temp/latest-version.txt
 
 # # Get current version
 # #CURRENT_VERSION=`/opt/app/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
