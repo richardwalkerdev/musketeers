c38f28ef78c041c18cdfa4d1731eaca147918fa3
Richard Walker <rwalker@redhat.com>
2020-03-25 12:09:36 +0000

    testing custom image

--- scripts/compare-versions.sh
+++ scripts/compare-versions.sh
@@ -7,19 +7,20 @@
 #echo "some text" > testing-working-dir.txt
 
 echo "ACCOUNT_PATH = ${ACCOUNT_PATH}"
+echo "OCP_PATH = ${OCP_PATH}"
 
 LATEST="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.3/release.txt"
 
 LATEST_VERSION=`curl -s ${LATEST} | grep Version: | sed 's/ *Version: *//g'`
 
-echo $LATEST_VERSION > /opt/temp/latest-version.txt
+echo $LATEST_VERSION > ${OCP_PATH}/latest-version.txt
 
 # Get current version
-CURRENT_VERSION=`/opt/temp/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
-echo $CURRENT_VERSION > /opt/temp/current-version.txt
+CURRENT_VERSION=`${OCP_PATH}/openshift-install version | head -n 1 | awk '{print $2}' | sed 's/ *v *//g'`
+echo $CURRENT_VERSION > ${OCP_PATH}/current-version.txt
 
 if [[ ${CURRENT_VERSION} != ${LATEST_VERSION} ]]; then
-    echo "Newer version found, deploy required" && touch ${ACCOUNT_PATH}/continue;
+    echo "Newer version found, deploy required" && touch ${OCP_PATH}/continue;
 else
-    echo "Matching versions, no changes required" && touch ${ACCOUNT_PATH}/stop;
+    echo "Matching versions, no changes required" && touch ${OCP_PATH}/stop;
 fi
